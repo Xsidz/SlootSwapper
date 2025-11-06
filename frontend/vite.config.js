5aa9fbec432a85d74c6a74a4473cb2553ae11f10
Xsidz <siddhesh.kabraa@gmail.com>
2025-11-06 20:00:34 +0530

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,13 +21,11 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         sourcemap: false,
+        target: 'esnext',
         rollupOptions: {
             output: {
                 manualChunks: undefined,
             },
         },
-    },
-    optimizeDeps: {
-        exclude: ['@rollup/rollup-linux-x64-gnu']
     }
 })
